bd2d9afd6afc2dd56cd44f6018fac7ffc7108516
Robert Wahler <robert@gearheadforhire.com>
2012-02-18 14:37:15 -0500

    add examples/rails_app
        cd examples
        rails new rails_app --skip-bundle -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '12d9755ad3966842900893ea9108150f1de7809859db917daea45f71bed2cc3c0da6cee0e86e6bb666ed050df603256dfed8f3e63b6f2d23ed602542ae95ad53'
