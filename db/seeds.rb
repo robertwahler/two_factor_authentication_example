c0e161b2ac7ad33bd9b2908995fe0da1bf60aaa2
Andy Koch <andy@shortstacklab.com>
2012-09-06 14:55:56 -0700

    space confuses my Ruby, thinks it's a syntax error

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
-user = User.create! (
+user = User.create!(
   :login  => 'admin',
   :email  => 'admin@example.com',
   :first_name  => 'admin',
